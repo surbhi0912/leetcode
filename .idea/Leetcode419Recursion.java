986f8616d79b2f093382f850cc307cee20c4deb1
Surbhi Agarwal <surbhi.agarwal@nurture.farm>
2023-11-23 22:55:27 +0530

    recursion

--- .idea/Leetcode419Recursion.java
+++ .idea/Leetcode419Recursion.java
@@ -1,6 +1,6 @@
 class Solution {
     public:
-    void explore(vector<vector<char>>& board, int i, int j) {
+    void dfs(vector<vector<char>>& board, int i, int j) {
         // Base cases for recursion
         if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == '.') {
             return;
@@ -10,10 +10,10 @@ class Solution {
         board[i][j] = '.';
 
         // Recursively explore the neighbors (up, down, left, right)
-        explore(board, i - 1, j);
-        explore(board, i + 1, j);
-        explore(board, i, j - 1);
-        explore(board, i, j + 1);
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
     }
     int countBattleships(vector<vector<char>>& board) {
         int m = board.size();
@@ -28,7 +28,7 @@ class Solution {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] == 'X') {
                     count++;  // Found a battleship, increment count
-                    explore(board, i, j);  // Recursively explore the battleship
+                    dfs(board, i, j);  // Recursively explore the battleship
                 }
             }
         }
