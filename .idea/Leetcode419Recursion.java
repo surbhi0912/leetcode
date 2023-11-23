a2a502bc19abad0902a2e6b7d0ab9296c10c7bd8
Surbhi Agarwal <surbhi.agarwal@nurture.farm>
2023-11-23 23:00:44 +0530

    recursion

--- .idea/Leetcode419Recursion.java
+++ .idea/Leetcode419Recursion.java
@@ -1,18 +1,18 @@
 public class Solution {
-    private void dfs(char[][] board, int i, int j) {
+    private void dfs(char[][] board, int row, int col) {
         // Base cases for recursion
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '.') {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] == '.') {
             return;
         }
 
         // Mark the cell as visited
-        board[i][j] = '.';
+        board[row][col] = '.';
 
         // Recursively explore the neighbors (up, down, left, right)
-        dfs(board, i - 1, j);
-        dfs(board, i + 1, j);
-        dfs(board, i, j - 1);
-        dfs(board, i, j + 1);
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
     }
     public int countBattleships(char[][] board) {
         int m = board.length;
@@ -21,17 +21,17 @@ public class Solution {
         }
 
         int n = board[0].length;
-        int count = 0;
+        int numBattleships = 0;
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'X') {
-                    count++;  // Found a battleship, increment count
-                    dfs(board, i, j);  // Recursively explore the battleship
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                if (board[row][col] == 'X') {
+                    numBattleships++;  // Found a battleship, increment count
+                    dfs(board, row, col);  // Recursively explore the battleship
                 }
             }
         }
 
-        return count;
+        return numBattleships;
     }
 }
